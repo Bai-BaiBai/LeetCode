a99bfb9a790516ff5b0aea277f2cf1617a8e0df5
Bai-BaiBai <877049473@qq.com>
2019-06-16 09:21:32 +0800

    20-有效的括号

---
+++ src/Solution_20.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * 20. 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ */
+public class Solution_20 {
+
+    public boolean isValid(String s) {
+
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']','[');
+        map.put('}', '{');
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }else {
+                if (stack.isEmpty() || map.get(c) != stack.pop()) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
