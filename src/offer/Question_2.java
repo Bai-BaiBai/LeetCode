17341c3a9ed6aa56c654ccc7ba7d336bfd35d470
Bai-BaiBai <877049473@qq.com>
2019-07-06 20:19:19 +0800

    单例模式的三种写法

--- src/offer/Question_2.java
+++ src/offer/Question_2.java
@@ -33,3 +33,27 @@ public class Question_2 {
     }
 }
 
+/*
+public class Question_2{
+
+    //防止指令重排序：insrance在没有初始化的情况下，内存已经分配给它
+    private static volatile Question_2 instance;
+
+    private Question_2(){}
+
+    public static Question_2 getInstance(){
+
+        if (instance == null){
+            synchronized (Question_2.class){
+                if (instance == null){
+                    instance = new Question_2();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+}
+*/
+
