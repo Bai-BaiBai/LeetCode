dfcb52f72a5a24cc774941a77ddac89338acee0d
Bai-BaiBai <877049473@qq.com>
2019-08-04 09:04:22 +0800

    二叉树的中序遍历

--- src/LeetCode/Solution_94.java
+++ src/LeetCode/Solution_94.java
@@ -2,6 +2,7 @@ package LeetCode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 二叉树的中序遍历
@@ -12,6 +13,28 @@ public class Solution_94 {
         int val;TreeNode left;
         TreeNode right;
         TreeNode(int x) { val = x; }
+    }
+
+    // 非递归解法
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()){
+            while (root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            if (!stack.isEmpty()){
+                root = stack.pop();
+                result.add(root.val);
+                root = root.right;
+            }
+        }
+
+        return result;
     }
 
     // 递归解法
