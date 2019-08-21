15d3a64889b1f097e3efdf0d3c4686d2c3c9de46
Bai-BaiBai <877049473@qq.com>
2019-08-21 11:03:42 +0800

    98-验证二叉搜索树

--- src/LeetCode/Solution_98.java
+++ src/LeetCode/Solution_98.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -39,5 +41,48 @@ public class Solution_98 {
         }
 
         return true;
+    }
+
+    public void inorder(TreeNode root){
+
+        Stack<TreeNode> stack = new Stack<>();
+        while (root!= null || !stack.isEmpty()){
+            while (root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            if (!stack.isEmpty()){
+                root = stack.pop();
+                System.out.print(root.val + " ");
+                root = root.right;
+            }
+        }
+    }
+
+    public void preorder(TreeNode root){
+
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) stack.push(root);
+        while (!stack.isEmpty()){
+            root = stack.pop();
+            System.out.println(root.val);
+
+            if (root.left != null) stack.push(root.left);
+            if (root.right != null) stack.push(root.right);
+        }
+    }
+
+    public void levelOrder(TreeNode root){
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        if (root != null) queue.add(root);
+
+        while (!queue.isEmpty()){
+            root = queue.remove();
+            System.out.println(root.val);
+
+            if (root.left != null) queue.add(root.left);
+            if (root.right != null) queue.add(root.right);
+        }
     }
 }
